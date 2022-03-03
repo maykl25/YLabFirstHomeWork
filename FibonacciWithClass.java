6ca014dc7e58d4f3d2b2075e06028be29b73355f
Сергей Шураев <70658716+maykl25@users.noreply.github.com>
2022-03-03 14:04:42 +0200

    Add files via upload

--- FibonacciWithClass.java
+++ FibonacciWithClass.java
@@ -1,29 +1,28 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class FibonacciWithClass {
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
         int number = Integer.parseInt(reader.readLine());
         Fibonachi fib = new Fibonachi();
-        ArrayList<Long> numbers = fib.memoryFibonacci(number);
-        for (long everyNumber : numbers) {
-            System.out.print(everyNumber + " ");
-        }
+        HashMap<Integer, Long> numbers = fib.memoryFibonacci(number);
+        System.out.println(numbers);
     }
 }
 
 class Fibonachi {
-    public ArrayList<Long> fibNumbers = new ArrayList<>();
 
-    public ArrayList<Long> memoryFibonacci(int n) {
-        fibNumbers.add(0L);
-        fibNumbers.add(1L);
+    public HashMap<Integer, Long> table = new HashMap<>();
+
+    public HashMap<Integer, Long> memoryFibonacci(int n) {
+        table.put(0, 0L);
+        table.put(1, 1L);
         for (int i = 2; i < n; i++) {
-            fibNumbers.add(fibNumbers.get(i - 1) + fibNumbers.get(i - 2));
+            table.put(i, table.get(i-1) + table.get(i - 2));
         }
-        return fibNumbers;
+        return table;
     }
 }
